237965bdf1de541c43d9f10b348e5271e591b436
Tobias Bengfort <bengfort@mpib-berlin.mpg.de>
2021-11-03 13:08:24 +0100

    bump version to 0.0.1

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     url='https://github.com/xi/django-model-stats',
     author='Tobias Bengfort',
     author_email='tobias.bengfort@posteo.de',
-    version='0.0.0',
+    version='0.0.1',
     license='MIT',
     packages=find_packages(exclude=['tests']),
     include_package_data=True,
